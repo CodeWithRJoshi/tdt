c6830e89af14799ac281e838023f179a33be9ca2
Rabindra Joshi <mail@rjoshi.net>
2020-04-05 01:45:18 +0545

    Remove skx

--- beta/test_000_main.py
+++ beta/test_000_main.py
@@ -45,10 +45,10 @@ class MainTest(unittest.TestCase):
         print(dir_path)
 
         for tf in test_files:
-            # formatted strings don't well with GitHub actions! :D
-            py_script = f"python {dir_path}/test_{tf}"
-            os.system(py_script)
-            # os.system("python" + " " + dir_path + "/test_" + tf)
+            # formatted strings don't well with GitHub actions! :(
+            # py_script = f"python {dir_path}/test_{tf}"
+            # os.system(py_script)
+            os.system("python" + " " + dir_path + "/test_" + tf)
 
 
 
